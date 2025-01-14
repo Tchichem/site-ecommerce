eca27d23cec3db4ba1982b66e0f1adedf78f678d
Tchichem <152623185+Tchichem@users.noreply.github.com>
2025-01-14 14:34:38 +0100

    define

--- inc/init.php
+++ inc/init.php
@@ -1,7 +1,7 @@
 <?php
 
 // Connexion à la BDD
-$pdo = new PDO('mysql:host=localhost;dbname=shop','hichemfecommerce','Afpa06130', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=hichemfecommerce.mysql.db;dbname=hichemfecommerce','hichemfecommerce','Afpa06130', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 require_once("fonction.php");
 
